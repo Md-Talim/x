4387faef578fe8821f5bd8dccf274662b97a33c8
Md. Talim <talimmohammad116@gmail.com>
2025-11-21 19:30:43 +0530

    Add Java runner

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func runKotlin(file string) {
@@ -53,6 +54,31 @@ func runCpp(file string) {
 	os.Remove("main")
 }
 
+func runJava(file string) {
+	baseName := filepath.Base(file)
+	className := strings.TrimSuffix(baseName, filepath.Ext(baseName))
+
+	// Compile
+	compileCmd := exec.Command("javac", file)
+	compileCmd.Stdout = os.Stdout
+	compileCmd.Stderr = os.Stderr
+	if err := compileCmd.Run(); err != nil {
+		fmt.Println("Compilation failed:", err)
+		os.Exit(1)
+	}
+
+	// Run
+	runCmd := exec.Command("java", className)
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+	if err := compileCmd.Run(); err != nil {
+		fmt.Println("Execution failed:", err)
+		os.Exit(1)
+	}
+
+	os.Remove(className + ".class")
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ktrun <file.kt>")
@@ -67,6 +93,8 @@ func main() {
 		runKotlin(file)
 	case ".cpp", ".cc":
 		runCpp(file)
+	case ".java":
+		runJava(file)
 	default:
 		fmt.Println("File not supported")
 		os.Exit(1)
